3b9b786954c1cb4d6b3c8f1ad2fea2bfc1fee409
Pranav Rao <pranav.rao@amadeus.com>
2018-03-05 13:57:33 -0500

    More cleanup

--- src/main/java/FoodTruckFinder.java
+++ src/main/java/FoodTruckFinder.java
@@ -20,7 +20,8 @@ public class FoodTruckFinder {
   private final static int LIMIT = 1000; // API limit is 1000 results per call
   private final static int PAGE_SIZE = 10; // Setting the user display limit of 10 results per page
   private static final String BASE_URL = "http://data.sfgov.org/resource/bbb8-hzi6.json";
-  private static Queue<FoodTruckModel> mainQueue = new LinkedList<>(); //
+  // Queue to keep track of the results to be displayed next to the user
+  private static Queue<FoodTruckModel> mainQueue = new LinkedList<>();
   private static int offset = 0;
 
   public static void main(String[] args) throws IOException {
@@ -55,6 +56,9 @@ public class FoodTruckFinder {
     }
   }
 
+  /**
+   * Display the Food Truck name and it's address in a 2 column table to the output console
+   */
   private static void displayConsoleOutput(List<FoodTruckModel> results) {
     String[] columnNames = {"NAME", "ADDRESS"};
     String[][] data = new String[results.size()][2];
@@ -72,10 +76,11 @@ public class FoodTruckFinder {
 
   /**
    * This method calls the FoodTruck API and returns a list of FoodTruck Model objects mapped from
-   * the API JSON response
+   * the JSON response
    */
   private static List<FoodTruckModel> getData(int offset) throws IOException {
     StringBuilder result = new StringBuilder();
+    // Limit is the API response size limit and the offset is the index from which the results are returned
     URL url = new URL(BASE_URL + "?$limit=" + LIMIT + "&$offset=" + offset);
     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
     conn.setRequestMethod("GET");
@@ -90,6 +95,10 @@ public class FoodTruckFinder {
         });
   }
 
+  /**
+   * Return the filtered result list containing only the food trucks open during program execution
+   * time and date
+   */
   private static List<FoodTruckModel> getFilteredList(List<FoodTruckModel> list) {
     List<FoodTruckModel> resultList = new ArrayList<>();
 
@@ -101,6 +110,7 @@ public class FoodTruckFinder {
     for (FoodTruckModel item : list) {
       LocalTime startTime = LocalTime.parse(item.getOpeningTime(), dateTimeFormatter);
       LocalTime endTime = LocalTime.parse(item.getClosingTime(), dateTimeFormatter);
+      // Filter the results to only have food trucks open right now
       if (item.getOpenDay().equalsIgnoreCase(dayOfWeek) && startTime.isBefore(localTime)
           && endTime.isAfter(localTime)) {
         resultList.add(item);
@@ -110,30 +120,41 @@ public class FoodTruckFinder {
     return resultList;
   }
 
-  // always return list of size count
+  /**
+   * Return a list of Food Trucks open at the time and day of program execution of size PAGE_SIZE or
+   * lower for display
+   */
   private static List<FoodTruckModel> getNextResults() throws IOException {
 
-    List<FoodTruckModel> result = new ArrayList<>();
-    List<FoodTruckModel> rawList;
-    List<FoodTruckModel> filteredList;
+    List<FoodTruckModel> result = new ArrayList<>(); //
+    List<FoodTruckModel> rawList; // Data returned straight from the API call
+    List<FoodTruckModel> filteredList; // Filtered raw list data containing only relevant food trucks open at the program execution time
 
+    // We populate the response till it reaches the Display size limit.
+    // It can happen that we get more relevant results than the page size limit in the first
+    // iteration of the loop and we still populate the response list and break out of the loop
     while (result.size() < PAGE_SIZE && offset >= 0) {
       rawList = getData(offset);
       filteredList = getFilteredList(rawList);
       result.addAll(filteredList);
-
+      // If the API call response size is lower than the API response limit, it means that there are no more food trucks to query
       if (rawList.size() < LIMIT) {
         offset = -1;
         break;
       }
+      // Set the offset index for the next API call.
+      // For eg. if 1000 food trucks were queried earlier for offset 0 (0-999 results), we query again from offset index 1000 (1000-1999 results) and so on
       offset += LIMIT;
     }
     return result;
   }
 
+  /**
+   * Return the result set for display. It has size <= the display page size
+   */
   private static List<FoodTruckModel> getResultsForDisplay() throws IOException {
 
-    List<FoodTruckModel> myList = new ArrayList<>();
+    List<FoodTruckModel> displayList = new ArrayList<>();
 
     if (mainQueue.size() < PAGE_SIZE) {
       mainQueue.addAll(getNextResults());
@@ -141,8 +162,8 @@ public class FoodTruckFinder {
 
     int size = Math.min(PAGE_SIZE, mainQueue.size());
     for (int i = 0; i < size; i++) {
-      myList.add(mainQueue.remove());
+      displayList.add(mainQueue.remove());
     }
-    return myList;
+    return displayList;
   }
 }
